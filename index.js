967620f2c39ee9ae0da9f50040085e8ab3c8f765
Aleksandr Savchenko <titamoto@gmail.com>
2023-05-02 14:27:00 -0700

    add event listener on form submit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const findBrewery = document.getElementById("find-brewery");
-const locationButton = document.getElementById("location-button")
-locationButton.addEventListener("click", getLocation)
+const findBreweryForm = document.getElementById("find-brewery");
+const locationButton = document.getElementById("location-button");
+locationButton.addEventListener("click", getLocation);
+findBreweryForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    findBrewery(e.target["find-brewery-input"].value)});
 
 function getLocation() {
     navigator.geolocation.getCurrentPosition(position => console.log(position.coords.latitude + " " + position.coords.longitude))
+} 
+
+function findBrewery(input) {
+  console.log(input);
 }
